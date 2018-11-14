9bc869d2fb8bbdcce7d7c24dc5c82df1c660f422
mia94 <mia_3@hanmail.net>
2018-11-14 17:27:24 +0900

    connectionTest 완료

---
+++ src/test/java/jdbc_study_coffee/ConnectionProviderTest.java
@@ -0,0 +1,41 @@
+package jdbc_study_coffee;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ConnectionProviderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println();
+		LogUtil.prnLog("Start ConnectionProviderTest");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println();
+		LogUtil.prnLog("End ConnectionProviderTest");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println();
+	}
+
+	@Test
+	public void test() {
+		try(Connection connection = ConnectionProvider.getConnection()){
+			LogUtil.prnLog(connection.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
