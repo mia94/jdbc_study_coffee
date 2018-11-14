ed94479348d62ccc4c76b8e5f5a063b02185d0bb
bonnie <bonnie@DESKTOP-V5VBT80>
2018-11-15 00:04:11 +0900

    Sale, ProductDaoTest

--- src/test/java/jdbc_study_coffee/ProductDaoTest.java
+++ src/test/java/jdbc_study_coffee/ProductDaoTest.java
@@ -9,19 +9,25 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import java_swing_coffee.dao.ProductDao;
+import java_swing_coffee.dao.ProductDaoImpl;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProductDaoTest {
-
+	static ProductDao dao;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		System.out.println();
 		LogUtil.prnLog("Start ConnectionProviderTest");
+		dao = new ProductDaoImpl();
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		System.out.println();
 		LogUtil.prnLog("End ConnectionProviderTest");
+		dao = null;
 	}
 
 	@Before
@@ -30,8 +36,44 @@ public class ProductDaoTest {
 	}
 
 	@Test
-	public void test() {
+	public void test01SelectProductByAll() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void test02InsertProduct() {
+		
+	}
+	
+	@Test
+	public void test03UpdateProduct() {
+		
+	}
+	
+	@Test
+	public void test03DeleteProduct() {
+		
+	}
+	
+	@Test
+	public void test05SelectProductByNo() {
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
