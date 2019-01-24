ae999ef0bd8654031f63868d361e3d14cd277deb
mia94 <mia_3@hanmail.net>
2019-01-24 17:29:21 +0900

    시도중

--- src/main/java/jdbc_study_coffee/ui/ProductRankPanel.java
+++ src/main/java/jdbc_study_coffee/ui/ProductRankPanel.java
@@ -38,8 +38,8 @@ public class ProductRankPanel extends JPanel {
 //		setAlignWidth();
 	}
 	private void setAlignWidth() {
-		tableCellAlignment(SwingConstants.CENTER,0,1,2,3,4,5,6,7,8,9);
-		//tableCellAlignment(SwingConstants.RIGHT,3,4,5,6,7,8,9);
+		tableCellAlignment(SwingConstants.CENTER,0,1,2);
+		tableCellAlignment(SwingConstants.RIGHT,3,4,5,6,7,8,9);
 		tableSetWidth(80,100,200,200,200,300,300,300,200,300);
 		
 	}
